aad460b9afcb675d57133fc71f35da85cb870575
Umer Sagheer <mhadisb1212@gmail.com>
2023-04-19 11:30:30 +0500

    Tech section Done

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,19 @@
+import { BallCanvas } from './canvas'
+import { SectionWraper } from '../HOC'
+import { technologies } from '../constants'
+
 import React from 'react'
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon}/>
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWraper(Tech,"")
